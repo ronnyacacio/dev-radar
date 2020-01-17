3fd670d6d8ef0087091cad70e02783fa9a73486c
Ronny Ac[acio <ronnyacacio27@gmail.com>
2020-01-17 18:11:10 -0300

    Web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,7 +10,8 @@ mongoose.connect('mongodb://localhost:27017/devs-map', {
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(3001);
